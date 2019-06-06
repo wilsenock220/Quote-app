cd494018cf12d9a7fe7b23c34699f49892b73980
wilsenock220 <wilsenock220@gmail.com>
2019-06-06 15:47:28 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-  public showDescription:boolean;
-  constructor(public id: number, public author: string, public quote: string,public description: string, public completeDate: Date){
-    this.showDescription=false
-  }
-}
